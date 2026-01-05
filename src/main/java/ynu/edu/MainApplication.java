46862db75f98da3a4279ec36b64aaca1b59718bb
11 <2528734921@qq.com>
2026-01-05 23:41:08 +0800

    主程序

--- src/main/java/ynu/edu/MainApplication.java
+++ src/main/java/ynu/edu/MainApplication.java
@@ -1,13 +1,15 @@
 package ynu.edu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@SpringBootApplication
+@MapperScan("ynu.edu.mapper")
+@EnableScheduling
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class,args);
-        System.out.println("=====================================");
-        System.out.println("能耗监测平台后端启动成功！");
-        System.out.println("接口文档地址：http://localhost:8080/api/doc.html");
-        System.out.println("=====================================");
     }
 }
