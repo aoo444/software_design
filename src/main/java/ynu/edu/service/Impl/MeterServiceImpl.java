2e013b4cb3d3c1062828e43e95e60c3726166e6a
11 <2528734921@qq.com>
2026-01-07 11:57:02 +0800

    新增检查设备编号是否重复

--- src/main/java/ynu/edu/service/Impl/MeterServiceImpl.java
+++ src/main/java/ynu/edu/service/Impl/MeterServiceImpl.java
@@ -1,8 +1,9 @@
 package ynu.edu.service.Impl;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ynu.edu.exception.BusinessException;
 import ynu.edu.mapper.MeterMapper;
 import ynu.edu.po.Meter;
 import ynu.edu.service.MeterService;
@@ -11,6 +12,15 @@ import ynu.edu.service.MeterService;
 public class MeterServiceImpl extends ServiceImpl<MeterMapper,Meter> implements MeterService {
     @Override
     public boolean addMeter(Meter meter) {
+        // 检查设备编号是否重复
+        Meter existingMeter = this.getOne(Wrappers.<Meter>lambdaQuery()
+                .eq(Meter::getSn, meter.getSn())
+                .eq(Meter::getIs_deleted, 0));
+        
+        if (existingMeter != null) {
+            throw new BusinessException("设备编号已存在");
+        }
+        
         meter.setIs_deleted(0);
         // 输出调试信息
         System.out.println("新建设备信息：" + meter);
